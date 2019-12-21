85ffdfe2e93c39cb8ca51dc49e8da3240631b26f
Jaime Cárdenas Mesa <jaime.cardenas176@gmail.com>
2019-12-21 21:43:18 +0100

    drawing the screen (turtle)

--- space_invaders.py
+++ space_invaders.py
@@ -3,8 +3,21 @@ import os
 
 #Set up the screen
 window = turtle.Screen()
-window.bground("black")
-wn.title("Space Invaders")
+window.bgcolor("black")
+window.title("Space Invaders")
 
 #Draw border
-border_pen = Turtle.Turtle()
+border_pen = turtle.Turtle()
+border_pen.speed(0)
+border_pen.color("white")
+border_pen.penup()
+border_pen.setposition(-300, -300)
+border_pen.pendown()
+border_pen.pensize(3)
+
+for side in range(4):
+    border_pen.fd(600)
+    border_pen.lt(90)
+border_pen.hideturtle()
+
+delay = input("Press enter to finish.")
