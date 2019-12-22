9a0137032690449d5e04a1d119648560180ce7ae
Jaime Cárdenas Mesa <jaime.cardenas176@gmail.com>
2019-12-22 12:18:55 +0100

    moving added to the player

--- space_invaders.py
+++ space_invaders.py
@@ -2,11 +2,13 @@ import turtle
 import os
 
 #Set up the screen
+
 window = turtle.Screen()
 window.bgcolor("black")
 window.title("Space Invaders")
 
 #Draw border
+
 border_pen = turtle.Turtle()
 border_pen.speed(0)
 border_pen.color("white")
@@ -19,5 +21,40 @@ for side in range(4):
     border_pen.fd(600)
     border_pen.lt(90)
 border_pen.hideturtle()
+
+#Create the player
+
+player = turtle.Turtle()
+player.color("blue")
+player.shape("triangle")
+player.penup()
+player.speed(0)
+player.setposition(0, -250)
+player.setheading(90)
+
+playerspeed = 15
+
+#Moving the player right and left
+
+def move_right():
+    x = player.xcor()
+    x += playerspeed
+    if x  < -280:
+        x = -280
+    player.setx(x)
+
+
+def move_left():
+    x = player.xcor()
+    x -= playerspeed
+    if x  < -280:
+        x = -280
+    player.setx(x)
+
+#Creating keyboard bindings
+turtle.listen()
+turtle.onkey(move_left, "Left")
+turtle.onkey(move_right, "Right")
+
 
 delay = input("Press enter to finish.")
